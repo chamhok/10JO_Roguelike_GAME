cfc0a1101c08decd540b602bcd4a230ae9141e5f
shstcs <shstcs@naver.com>
2023-12-01 16:57:41 +0900

    [ADD] UI-Player connect
    -시간,경험치,골드,레벨 UI 연결.
    -현재 스테이지를 읽어올 수는 있어야 할듯.

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     private GameObject[] StageGrid;
     private Canvas StageUI;
     private Canvas curUI;
+    private GameObject curStageGrid;
+
     private Stage currentStage = Stage.Stage1;
 
     private void Awake()
@@ -19,22 +23,85 @@ public class UIManager : MonoBehaviour
     private void Start()
     {
         curUI = Instantiate(StageUI);
-        Instantiate(StageGrid[(int)currentStage]);
+        curStageGrid = Instantiate(StageGrid[(int)currentStage]);
 
         StartCoroutine(ShowStageName());
-
-        GameManager.Instance.OnStageClear.AddListener(StageUp);
-        GameManager.Instance.OnStageFail.AddListener(StageReset);
     }
 
-    private void StageUp()
+    private void Update()
     {
-        currentStage++;
+        if(currentStage != Stage.Start)
+        {
+            ShowData();
+        }
+    }
+   
+
+    private void ShowData()
+    {
+        ChangeStageName();
+        ShowTime();
+        ShowExp();
+        ShowGold();
+        ShowLevel();
     }
 
-    private void StageReset()
+    public void ChangeStageName()
     {
-        currentStage = Stage.Stage1;
+        switch (currentStage)   //GameManager의 변수 Static이라서 접근 불가..
+        {
+            case Stage.Stage1:
+                curUI.GetComponentInChildren<TMP_Text>().text = "검수지옥";
+                break;
+            case Stage.Stage2:
+                curUI.GetComponentInChildren<TMP_Text>().text = "나태지옥";
+                break;
+            case Stage.Stage3:
+                curUI.GetComponentInChildren<TMP_Text>().text = "흑암지옥";
+                break;
+            default: break;
+        }
+    }
+    //시간 표시
+    private void ShowTime()
+    {
+        int remainedtime = (int)(120f - GameManager.Instance.stageLapseTime);
+        curUI.transform.Find("Timer").GetComponent<TMP_Text>().text = (remainedtime / 60).ToSafeString() + " : " + (remainedtime % 60).ToSafeString();
+    }
+
+    //레벨 표시
+    private void ShowLevel()
+    {
+        curUI.GetComponentInChildren<Slider>().transform.Find("Level").GetComponent<TMP_Text>().text = "LV " + GameManager.Instance.player.level.ToString();
+    }
+
+    //경험치 표시, 최대 경험치 필요함. 일단 10으로 가정.
+    private void ShowExp()
+    {
+        curUI.GetComponentInChildren<Slider>().value = GameManager.Instance.player.exp / 10;
+    }
+
+    //골드 표시
+    private void ShowGold()
+    {
+        GameObject gold = curUI.transform.Find("Gold").gameObject;
+        if (gold != null)
+        {
+            TMP_Text GoldText = gold.GetComponentInChildren<TMP_Text>();
+            GoldText.text = GameManager.Instance.player.money.ToString();
+        }
+        else
+        {
+            Debug.Log("Gold is null");
+        }
+
+    }
+
+    //레벨업시 아이템 선택창 출력
+    private void SelectItem()
+    {
+        Time.timeScale = 0;
+
     }
 
     private IEnumerator ShowStageName()
@@ -64,5 +131,6 @@ enum Stage
 {
     Stage1,
     Stage2,
-    Stage3
+    Stage3,
+    Start
 }
