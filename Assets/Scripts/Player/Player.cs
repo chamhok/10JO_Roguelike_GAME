a48b294c32fe385151c10bb9ff4f7da48893a93a
GAMGDOG <chltjdwo98@gmail.com>
2023-12-01 13:40:53 +0900

    fix:player_hp_bar

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,16 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    int maxHp;
-    int hp;
-    int atk; //공격력 배율
+    [SerializeField] private Image hpBar;
+
+    public int maxHp;      //최대체력
+    public int hp;        //체력
+    public int atk; //공격력 배율
     public int speed;//이동속도 배율
-    int level;
-    int exp;
-    int money;
+    public int level;     //레벨
+    public int exp;        //경험치   
+    public int money;     //돈
 
     public Player()
     {
@@ -22,7 +26,7 @@ public class Player : MonoBehaviour
         exp = 0;     //현재 exp(게임 오버, 게임 클리어 시 초기화 - 스테이지 클리어 아님)
         money = 0;   //현재 gold(메인화면, 스테이터스 강화 화면에서 사용하는 것으로 maxHp, atk, speed를 영구적으로 증가)
                      //증가할때 마다 필요한 money 증가
-    }       
+    }
 
     public Player(PlayerData playerData)
     {
@@ -32,5 +36,15 @@ public class Player : MonoBehaviour
         level = playerData.level;
         exp = playerData.currentExp;
         money = playerData.money;
+    }
+
+    private void Update()
+    {
+        ChangeHpBar(hp);
+    }
+
+    private void ChangeHpBar(int hp) //현재 체력 체력바에 표시
+    {
+        hpBar.fillAmount = (float)hp / maxHp;
     }
 }
